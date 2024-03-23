68eb62e6614b12f45b430b401486079002d95189
LordOfTheSauce <yutong201111@gmail.com>
2024-03-23 18:39:45 -0400

    I've made the fetching of the data all efficient. The fetching will be done in the portfolio class instead of the stock class.

--- portfolio.py
+++ portfolio.py
@@ -1,40 +1,77 @@
 import yfinance as yf
 from stock import Stock
+import pandas as pd
 class Portfolio:
     """
     A class to represent a stock portfolio.
     """
     def __init__(self, stocks: list):
         """
-        Initializes a new Portfolio object with user-provided input.
+        Initializes a new Portfolio object with a list of Stock objects. Fetch data from Yahoo Finance.
         Args:
             stocks (list): A list of Stock objects.
         """
-        self.stocks = stocks
-        self.symbols = [stock.symbol for stock in stocks]
-        self.__calculate_values__()
+        self.stocks = {}
+        for stock in stocks:
+            self.stocks.update({stock.symbol: stock})
+        
+        if(len(self.stocks) == 0):
+            print("No stocks in the portfolio. Exiting.")
+            exit()
 
-    def __calculate_values__(self) -> None:
-        """
-        Calculates dependent values based on the provided input.
-        """
-        self.market_value = sum([stock.market_value for stock in self.stocks])
-        self.cost_price = sum([stock.cost_price for stock in self.stocks])
-        self.total_change = sum([stock.total_change for stock in self.stocks])
-        self.gain_loss = self.total_change / self.cost_price * 100
-        past_market_value = sum([stock.qty * stock.past_price for stock in self.stocks])
-        self.weekly_change = ((self.market_value - past_market_value) / past_market_value) * 100
-        self.pe_ratio = sum([stock.pe_ratio for stock in self.stocks if stock.pe_ratio is not None]) / len([stock.pe_ratio for stock in self.stocks if stock.pe_ratio is not None])
+        self.data5y= yf.download(list(self.stocks.keys()), period = "5y")
+        self.data1d= yf.download(list(self.stocks.keys()), period = "1d", interval = "1m")
 
-    def add_stock(self, stock: Stock) -> None:
+        if(len(self.stocks) == 1):
+            symbol = list(self.stocks.keys())[0]
+            self.data5y.columns = pd.MultiIndex.from_product([self.data5y.columns, [symbol]],names=['Price', 'Ticker'])
+            self.data1d.columns = pd.MultiIndex.from_product([self.data1d.columns, [symbol]],names=['Price', 'Ticker'])
+
+        self.checkStocks()
+        self.calculate_values()
+
+    def calculate_values(self) -> None:
         """
-        Adds a new stock to the portfolio.
-        Args:
-            stock (Stock): A Stock object to be added to the portfolio.
+        Update the price for each stock in the portfolio, and calculate dependent values.
         """
-        self.stocks.append(stock)
-        self.symbols.append(stock.symbol)
-        self.__calculate_values__()
+        for stock in self.stocks.values():
+            currentPrice = self.data1d['Adj Close'][stock.symbol][-1]
+            pastPrice = self.data5y['Adj Close'][stock.symbol][-6]
+            stock.calculate_stock_values(currentPrice, pastPrice)
+        if len(self.stocks) == 0:
+            print("No stocks in the portfolio.")
+            self.market_value = 0
+            self.cost_price = 0
+            self.total_change = 0
+            self.gain_loss = 0
+            past_market_value = 0
+            self.weekly_change = 0
+            self.pe_ratio = 0
+        else:
+            self.market_value = sum([stock.market_value for stock in self.stocks.values()])
+            self.cost_price = sum([stock.cost_price for stock in self.stocks.values()])
+            self.total_change = sum([stock.total_change for stock in self.stocks.values()])
+            self.gain_loss = self.total_change / self.cost_price * 100
+            past_market_value = sum([stock.qty * stock.past_price for stock in self.stocks.values()])
+            self.weekly_change = ((self.market_value - past_market_value) / past_market_value) * 100
+            self.pe_ratio = sum([stock.pe_ratio for stock in self.stocks.values() if stock.pe_ratio is not None]) / len([stock.pe_ratio for stock in self.stocks.values() if stock.pe_ratio is not None])
+
+    def checkStocks(self):
+        errorMsg = yf.shared._ERRORS
+        errorSymbols = list(errorMsg.keys())
+        for errorSymbol in errorSymbols:
+            print(errorSymbol + " does not have current price available. Removing from portfolio.")
+            self.remove_stock(errorSymbol)
+
+    # # def add_stock(self, stock: Stock) -> None:
+    #     """
+    #     Adds a new stock to the portfolio.
+    #     Args:
+    #         stock (Stock): A Stock object to be added to the portfolio.
+    #     """
+    #     self.stocks.append(stock)
+    #     self.symbols.append(stock.symbol)
+    #     self.__calculate_values__()
 
     def remove_stock(self, stock: Stock) -> None:
         """
@@ -42,15 +79,27 @@ class Portfolio:
         Args:
             stock (Stock): A Stock object to be removed from the portfolio.
         """
-        self.stocks.remove(stock)
-        self.symbols.remove(stock.symbol)
-        self.__calculate_values__()
+        del self.stocks[stock.symbol]
+        self.data1d = self.data1d.drop(stock.symbol, axis=1, level='Ticker')
+        self.data5y = self.data5y.drop(stock.symbol, axis=1, level='Ticker')
+        self.calculate_values()
+        
+    def remove_stock(self, symbol: str) -> None:
+        """
+        Removes a stock from the portfolio.
+        Args:
+            symbol (str): A Stock symbol to be removed from the portfolio.
+        """
+        del self.stocks[symbol]
+        self.data1d = self.data1d.drop(symbol, axis=1, level='Ticker')
+        self.data5y = self.data5y.drop(symbol, axis=1, level='Ticker')
+        self.calculate_values()
 
     def print_stocks(self) -> None:
         """
         Prints a summary of all stocks in the portfolio.
         """
-        for stock in self.stocks:
+        for stock in self.stocks.values():
             print(stock)
             print("")
     
--- stock.py
+++ stock.py
@@ -1,4 +1,3 @@
-from typing import Optional
 import yfinance as yf
 
 class Stock:
@@ -16,42 +15,27 @@ class Stock:
             cost_per_share (float): The cost per share of the stock.
         """
         self.symbol = symbol
-        self.ticker = yf.Ticker(symbol)
+        self.ticker = yf.Ticker(symbol,)
         self.info = self.ticker.info
         if 'symbol' not in self.info:
             raise ValueError(f"No data found for ticker: {self.symbol}")
         self.qty = qty
         self.cost_per_share = cost_per_share
         self.company_name = self.info['longName']
-        self.__calclate_values__()  # Calculate dependent values
+        self.cost_price = self.qty * self.cost_per_share
+        self.pe_ratio = self.info['trailingPE'] if 'trailingPE' in self.info else 0
 
-    def __calclate_values__(self) -> None:
+    def calculate_stock_values(self,currentPrice,pastPrice) -> None:
         """
         Calculates dependent values based on the provided input.
         """
-        self.cost_price: float = self.qty * self.cost_per_share
-        self.weekly_data = self.ticker.history(period="1wk")
-        self.daily_data = self.ticker.history(period="1d")
-
-        try:
-            self.current_price = self.daily_data['Close'].iloc[0]
-            self.past_price = self.weekly_data['Close'].iloc[0]
-            self.weekly_change = ((self.current_price - self.past_price) / self.past_price) * 100  # Percentage change
-            self.market_value = self.qty * self.current_price 
-            self.total_change = self.market_value - self.cost_price 
-            self.gain_loss = self.total_change/self.cost_price * 100 
-            self.pe_ratio = self.info['trailingPE'] if 'trailingPE' in self.info else 0
-
-        except IndexError:
-            self.current_price = 0
-            self.past_price = 0
-            self.weekly_change = 0
-            self.market_value = 0
-            self.total_change = 0
-            self.gain_loss = 0
-            self.pe_ratio = None
-            print(f"Warning: No current price found for {self.symbol}, setting all values to 0.")
-
+        self.current_price = currentPrice
+        self.past_price = pastPrice
+        self.weekly_change = ((self.current_price - self.past_price) / self.past_price) * 100  # Percentage change
+        self.market_value = self.qty * self.current_price 
+        self.total_change = self.market_value - self.cost_price 
+        self.gain_loss = self.total_change/self.cost_price * 100 
+        
     def __str__(self) -> str:
         """
         Returns a string representation of the Stock object.
@@ -61,19 +45,12 @@ class Stock:
             f"Shares: {self.qty}",
             f"Cost per Share: ${self.cost_per_share:.2f}",
             f"Cost Basis: ${self.cost_price:.2f}",
-        ]
-
-        if self.current_price != 0:
-            lines.extend([
-                f"Current Price: ${self.current_price:.2f}",
-                f"Market Value: ${self.market_value:.2f}",
-                f"Total Change: ${self.total_change:.2f}",
-                f"Gain/Loss: {self.gain_loss:.2f}%",
-                f"Weekly Change: {self.weekly_change:.2f}%",
-                f"PE Ratio: {self.pe_ratio}"
-            ])
-        else:
-            lines.append("Warning: No current price found for this stock.")
-
+            f"Current Price: ${self.current_price:.2f}",
+            f"Market Value: ${self.market_value:.2f}",
+            f"Total Change: ${self.total_change:.2f}",
+            f"Gain/Loss: {self.gain_loss:.2f}%",
+            f"Weekly Change: {self.weekly_change:.2f}%",
+            f"PE Ratio: {self.pe_ratio}"
+            ]
         return "\n".join(lines)
 
